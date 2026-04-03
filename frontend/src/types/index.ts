490cdf722efac0bf541260fa80d9580e04caa00c
NotAHuman1230 <jerichojohn@outlook.com>
2026-04-03 13:13:30 +0100

    Type fixing

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,5 +2,6 @@ import type { components } from "./api.d.ts";
 
 export type User = components["schemas"]["User"];
 export type Issue = components["schemas"]["Issue"];
+export type IssueQueryShow = components["schemas"]["IssueQueryShow"];
 export type CreateIssue = components["schemas"]["CreateIssue"];
 export type Report = components["schemas"]["Report"];
