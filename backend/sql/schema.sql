9b141335cd3b0dd93f8db45e7d9764ef552c9ef5
v-Morriss <williammorriss@outlook.com>
2026-04-06 00:27:29 +0100

    loading locations from csv

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,28 +1,26 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE locations (
+\copy locations (name, description) FROM 'bath_locations.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE IF NOT EXISTS locations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    latitude REAL,
-    longitude REAL,
-    building TEXT,
-    level INTEGER NOT NULL,
-    description TEXT,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
     search_vector tsvector
         GENERATED ALWAYS AS (
         to_tsvector('english',
-        coalesce(building, '')    || ' ' ||
+        coalesce(name, '')    || ' ' ||
         coalesce(description, '')
         )
         ) STORED
 );
 
-CREATE TABLE issues (
+CREATE TABLE IF NOT EXISTS issues(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title TEXT,
     description TEXT,
@@ -36,7 +34,7 @@ CREATE TABLE issues (
         ) STORED
 );
 
-CREATE TABLE reports (
+CREATE TABLE IF NOT EXISTS reports(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     issue_id UUID NOT NULL REFERENCES issues(id),
     reporter_id UUID NOT NULL REFERENCES users(id),
