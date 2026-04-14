6f59aa5b4c9c7d8eeac04a87728fe7c8b6f66593
v-Morriss <williammorriss@outlook.com>
2026-04-14 20:44:31 +0100

    better location/ tag loading

---
+++ backend/scripts/static_data.py
@@ -0,0 +1,52 @@
+import json, psycopg2
+import csv
+from dotenv import load_dotenv
+import os
+from psycopg2.extras import execute_batch
+import psycopg2.extensions
+
+type Connection = psycopg2.extensions.connection
+
+load_dotenv()
+url = "https://search.bath.ac.uk/s/search.json?collection=bath~sp-cms-production-locations&num_ranks=130"
+
+def load_locations() -> list[dict[str,str]]:
+    with open("locations.json") as locations_json:
+        data = json.load(locations_json)
+    locations = data["response"]["resultPacket"]["results"]
+    return [{"name" : row["listMetadata"]["t"][0], "department" : row["listMetadata"]["X"][0], "description": row["listMetadata"]["L"][0], "url": row["indexUrl"]} for row in locations]
+
+
+def load_tags() -> list[dict[str, str]]:
+    with open("tags.csv", 'r') as tag_file:
+        tags = csv.DictReader(tag_file)
+        next(tags) # skip title
+        return [tag for tag in tags]
+
+def set_tags_table(db: Connection, tags: list[dict[str,str]]) -> None:
+    with db.cursor() as cursor:
+        cursor.execute("TRUNCATE TABLE tags CASCADE")
+        execute_batch(cursor, "INSERT INTO tags (name) VALUES (%(name)s)", tags)
+
+    db.commit()
+    print(f"Loaded {len(tags)} tags")
+
+
+def set_loc_table(db: Connection, locations: list[dict[str,str]]) -> None:
+    with db.cursor() as cursor:
+        cursor.execute("TRUNCATE TABLE locations CASCADE")
+        execute_batch(cursor, "INSERT INTO locations (name, department, description, url) VALUES (%(name)s, %(department)s, %(description)s, %(url)s)", locations )
+    db.commit()
+    print(f"Loaded {len(locations)} locations")
+
+def main() -> None:
+    with psycopg2.connect(os.getenv("DATABASE_URL")) as db:
+        loaded = load_locations()
+        tags = load_tags()
+        print(loaded)
+        set_loc_table(db, loaded)
+        print(tags)
+        set_tags_table(db, tags)
+
+if __name__ == "__main__":
+    main()
