c1e949f7401771f366ab871483d5661d2ad0f067
v-Morriss <williammorriss@outlook.com>
2026-04-14 20:44:31 +0100

    better location/ tag loading

--- backend/sql/drop.sql
+++ backend/sql/drop.sql
@@ -1,5 +1,7 @@
 -- sql script to quickly discard the database (for testing along with schema.sql)
-DROP TABLE IF EXISTS reports CASCADE;
-DROP TABLE IF EXISTS issues  CASCADE;
-DROP TABLE IF EXISTS locations  CASCADE;
 DROP TABLE IF EXISTS users  CASCADE;
+DROP TABLE IF EXISTS locations  CASCADE;
+DROP TABLE IF EXISTS issues  CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+DROP TABLE IF EXISTS issue_tags CASCADE;
+DROP TABLE IF EXISTS reports CASCADE;
--- backend/sql/load_tags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR="$(dirname "$0")"
-psql "$DATABASE_URL" -U postgres -d knowledb -c "\copy tags (name) FROM '$DIR/tags.csv' DELIMITER ',' CSV HEADER;"
