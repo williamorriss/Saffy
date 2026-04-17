67463f0bbc7225c776a1720686cac0c100371012
v-Morriss <williammorriss@outlook.com>
2026-04-17 02:11:08 +0100

    err

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 export default {
     async fetch(request, env, _ctx) {
         try {
-            const apiRequest = request.url.match(".*\/api(.*)");
+            const apiRequest = request.url.match(/.*\/api(.*)/);
             if (apiRequest !== null) {
                 const backend = env.VITE_BACKEND_URL;
                 const apiPath = apiRequest[1];
-                console.log(apiPath);
-                const targetUrl = new URL("/api",apiPath, backend);
-                console.log(targetUrl);
+                const targetUrl = new URL("/api" + apiPath, backend);
 
                 const proxyRequest = new Request(targetUrl.toString(), request);
                 proxyRequest.headers.set("Host", targetUrl.hostname);
@@ -19,13 +17,16 @@ export default {
                 const response = await env.ASSETS.fetch(request);
 
                 if (response.status === 404) {
-                    const indexRequest = new Request(`${url.origin}/index.html`, request);
+                    const indexRequest = new Request(
+                        new URL("/index.html", request.url).toString(),
+                        request
+                    );
                     return await env.ASSETS.fetch(indexRequest);
                 }
 
                 return response;
             } catch (e) {
-                return new Response("Asset not found", {status: 404});
+                return new Response("Asset not found", { status: 404 });
             }
         } catch (e) {
             console.log(e);
