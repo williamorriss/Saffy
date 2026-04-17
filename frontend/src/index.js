614c440e61c3e58b4a42d3a6276f91b6d44ebad5
v-Morriss <williammorriss@outlook.com>
2026-04-17 01:34:01 +0100

    try catch + rename worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,33 @@
 export default {
     async fetch(request, env, _ctx) {
-        const url = new URL(request.url);
-
-        if (url.pathname.startsWith("/api")) {
-            const backend = env.VITE_BACKEND_URL;
-            const targetUrl = new URL(url.pathname + url.search, backend);
-
-            const proxyRequest = new Request(targetUrl.toString(), request);
-            proxyRequest.headers.set("Host", targetUrl.hostname);
-
-            return fetch(proxyRequest);
-        }
-
         try {
-            const response = await env.ASSETS.fetch(request);
+            const url = new URL(request.url);
 
-            if (response.status === 404) {
-                const indexRequest = new Request(`${url.origin}/index.html`, request);
-                return await env.ASSETS.fetch(indexRequest);
+            if (url.pathname.startsWith("/api")) {
+                const backend = env.VITE_BACKEND_URL;
+                const targetUrl = new URL(url.pathname + url.search, backend);
+
+                const proxyRequest = new Request(targetUrl.toString(), request);
+                proxyRequest.headers.set("Host", targetUrl.hostname);
+
+                return fetch(proxyRequest);
             }
 
-            return response;
+            try {
+                const response = await env.ASSETS.fetch(request);
+
+                if (response.status === 404) {
+                    const indexRequest = new Request(`${url.origin}/index.html`, request);
+                    return await env.ASSETS.fetch(indexRequest);
+                }
+
+                return response;
+            } catch (e) {
+                return new Response("Asset not found", {status: 404});
+            }
         } catch (e) {
-            return new Response("Asset not found", { status: 404 });
+            console.log(e)
+            return new Response("Server Error", { status: 500 });
         }
     },
 };
