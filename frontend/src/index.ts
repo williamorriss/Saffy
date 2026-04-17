0e32d9856c001a7e4e3f19a1a42ff8ae3d0447c6
v-Morriss <williammorriss@outlook.com>
2026-04-17 01:04:31 +0100

    fix

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,5 +1,9 @@
+export interface Env {
+    VITE_BACKEND_URL: string;
+}
+
 export default {
-    async fetch(request, env) {
+    async fetch(request: Request, env: Env) {
         const url = new URL(request.url);
 
         if (!url.pathname.startsWith("/api")) {
@@ -13,7 +17,7 @@ export default {
             return new Response("Backend not set in Env", { status: 500 });
         }
 
-        const targetUrl = RUST_BACKEND + url.pathname + url.search;
+        const targetUrl = backend + url.pathname + url.search;
 
         return fetch(new Request(targetUrl, request));
     }
